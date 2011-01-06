3a9b4a1b5096fde2f3844672ceb3b658af68544f
tyler <tyler.green2@gmail.com>
2011-01-05 18:47:21 -0600

    make grab and spy portable across osx and linux

--- utils.bash
+++ utils.bash
@@ -1,10 +1,25 @@
+function up {
+    for ((c=1; c <= $1; c++))
+    do
+	cd ..
+    done
+}
 
 function grab {
-    sudo apt-get install $1
+    if [ "$OSTYPE" == 'linux-gnu' ]; then
+	sudo apt-get install $1
+    else
+	sudo port install $1
+    fi
 }
 
 function spy {
-    apt-cache search $1
+    if [ "$OSTYPE" == 'linux-gnu' ]; then
+	echo "new spy"
+	apt-cache search $1
+    else
+	port search $1
+    fi
 }
 
 function gh {
